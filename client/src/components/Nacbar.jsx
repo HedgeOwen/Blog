a49d43fd6d28887dc972b46dfe0ed8d112379750
Owen <hedgeowen2005@gmail.com>
2025-05-14 05:57:22 +0800

    Add Navbar and Footer

--- client/src/components/Nacbar.jsx
+++ client/src/components/Nacbar.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
+import Logo from "../img/logo.jpg"
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='navbar'>
+            <div className='container'>
+                <div className='logo'>
+                    <img src={Logo} alt="logo" />
+                </div>
+                <div className='links'>
+                    <Link className='link' to="/?cat=blog">
+                        <h4>Blog</h4>
+                    </Link>
+                    <Link className='link' to="/?cat=about">
+                        <h4>about</h4>
+                    </Link>
+                    <Link className='link' to="/?cat=about">
+                        <h4>about</h4>
+                    </Link>
+                    <Link className='link' to="/?cat=about">
+                        <h4>about</h4>
+                    </Link>
+                    <Link className='link' to="/?cat=about">
+                        <h4>about</h4>
+                    </Link>
+                    <Link className='link' to="/?cat=about">
+                        <h4>about</h4>
+                    </Link>
+                    <span>Owen</span>
+                    <span>Logout</span>
+                    <span className='write'>
+                        <Link className="link" to='/write'>Write</Link>
+                    </span>
+                </div>
+            </div>
+        </div>
     )
 }
 
