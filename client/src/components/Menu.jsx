370eacbb944d6906055230f061c51af19a54a91d
Owen <hedgeowen2005@gmail.com>
2025-06-05 06:52:10 +0800

    Add Single page

---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Logo from "../img/logo.jpg"
+
+const Menu = () => {
+    const posts = [
+        {
+            id: 1,
+            title: "Test",
+            desc: "test",
+            img: Logo,
+        },
+        {
+            id: 2,
+            title: "Test",
+            desc: "test",
+            img: Logo,
+        },
+        {
+            id: 3,
+            title: "Test",
+            desc: "test",
+            img: Logo,
+        },
+    ]
+    return (
+        <div className='menu'>
+            <h1>Other posts you may like</h1>
+            {posts.map(post=>(
+                <div className='post' key={post.id}>
+                    <img src={post.img} alt=""/>
+                    <h2>{post.title}</h2>
+                    <button>Read More</button>
+                </div>    
+            ))}
+        </div>
+    )
+}
+
+export default Menu
--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import Logo from "../img/logo.jpg"
+import {Link} from 'react-router-dom'
+import Menu from "../components/Menu"
 
 const Single = () => {
     return (
-        <div>Single</div>
+        <div className='single'>
+            <div className='content'>
+                <img src={Logo}></img>
+            <div className='user'>
+                <img src={Logo}></img>
+            <div className='info'>
+                <span>Owen</span>
+                <p>Posted 2 days ago</p>
+            </div>
+            <div className='edit'>
+                <Link to={`/write?edit=1`}>
+                    <button>Edit</button>
+                </Link>
+                <button>Delete</button>
+            </div>
+            </div>
+            <h1>My test title</h1>
+            <p>My test content My test content My test content My test content My test content My test content My test content My test content My test content My test content My test content My test content My test content My test content My test content
+                <br/><br/>My test content My test content My test content My test content My test content My test content My test content My test content My test content My test content My test content My test content
+                
+            </p>
+            </div>
+            <Menu />
+        </div>
     )
 }
 
